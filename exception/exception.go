d267d63eaff67e62e15dfa9dc3a0c0122b53c64f
yangxiancheng@commasmart.com <yangxiancheng@commasmart.com>
2019-09-29 11:32:36 +0800

    up

--- exception/exception.go
+++ exception/exception.go
@@ -1,15 +1,73 @@
 package exception
 
+import (
+	"fmt"
+	"os"
+	"time"
+	"os/exec"
+	"path/filepath"
+	"runtime/debug"
+)
 
-/**
-	异常捕获(暂时还没完善)
-	@author Bill
- */
-func Try(fun func(), handler func()) {
-	defer func() {
-		if err := recover(); err != nil {
-			handler()
-		}
+/*
+  用于抓取错误记录打印(panic信息，不可度量的)
+  注意：每个 goroutine  开始部分都需要写上:defer mycatch.Dmp()
+  类似于
+	go func(){
+		defer mycatch.Dmp()
 	}()
-	fun()
+*/
+
+func Dmp() {
+	errstr := ""
+	if err := recover(); err != nil {
+		errstr += (fmt.Sprintf("%v\r\n", err)) //输出panic信息
+		errstr += ("--------------------------------------------\r\n")
+	}
+
+	errstr += (string(debug.Stack())) //输出堆栈信息
+	OnPrintErr(errstr)
+}
+
+func OnPrintErr(errstring string) {
+	path := GetModelPath() + "/err"
+	if !PathExists(path) {
+		os.MkdirAll(path, os.ModePerm) //生成多级目录
+	}
+
+	now := time.Now()                                               //获取当前时间
+	pid := os.Getpid()                                              //获取进程ID
+	time_str := now.Format("2006-01-02")                            //设定时间格式
+	fname := fmt.Sprintf("%s/panic_%s-%x.log", path, time_str, pid) //保存错误信息文件名:程序名-进程ID-当前时间（年月日时分秒）
+	fmt.Println("panic to file ", fname)
+
+	f, err := os.OpenFile(fname, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	f.WriteString("=========================" + now.Format("2006-01-02 15:04:05 ========================= \r\n"))
+	f.WriteString(errstring) //输出堆栈信息
+	f.WriteString("=========================end=========================")
+}
+
+//获取目录地址
+func GetModelPath() string {
+	file, _ := exec.LookPath(os.Args[0])
+	path := filepath.Dir(file)
+	path, _ = filepath.Abs(path)
+
+	return path
+}
+
+func PathExists(path string) (bool) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
 }
