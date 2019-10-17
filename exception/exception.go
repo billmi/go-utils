d838449e26c4683e224faa39302574d92d40f84e
yangxiancheng@commasmart.com <yangxiancheng@commasmart.com>
2019-10-17 09:49:41 +0800

    exception throw

---
+++ exception/exception.go
@@ -0,0 +1,16 @@
+package recover
+
+/**
+	ThrowException
+	@author : Bill
+	异常统一抛出处理
+ */
+
+func ThrowException(execCall func()) {
+	defer func() {
+		if err := recover(); err != nil {
+			//Do Something
+		}
+	}()
+	execCall()
+}
