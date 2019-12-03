f54fa1a1f8d99bf0c7c175daa5ff120a0e4a0c96
yangxiancheng@commasmart.com <yangxiancheng@commasmart.com>
2019-12-03 14:17:58 +0800

    update

--- array/array.go
+++ array/array.go
@@ -1,5 +1,7 @@
 package array
 
+import "strconv"
+
 //合并数组
 func Copy(dest []interface{}, src []interface{}) (result []interface{}) {
 	result = make([]interface{}, len(dest)+len(src))
@@ -13,3 +15,33 @@ func DeleteArray(src []interface{}, index int) (result []interface{}) {
 	result = append(src[:index], src[(index+1):]...)
 	return
 }
+
+// []string => []int
+func ArrayStr2Int(data []string)[]int{
+	var (
+		arr = make([]int,0)
+	)
+	if len(data) == 0{
+		return arr
+	}
+	for i,_ := range data{
+		var num,_ = strconv.Atoi(data[i])
+		arr = append(arr, num)
+	}
+	return arr
+}
+
+// []int => []string
+func ArrayInt2Str(data []int)[]string{
+	var (
+		arr = make([]string,0)
+	)
+	if len(data) == 0{
+		return arr
+	}
+	for i,_ := range data{
+		arr = append(arr, strconv.Itoa(data[i]))
+	}
+	return arr
+}
+
